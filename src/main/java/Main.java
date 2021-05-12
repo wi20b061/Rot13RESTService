85ee68617c8b5b6b74af5891584f58b3dce7d0f9
Fiona K <fiona5@gmx.at>
2021-05-12 08:19:11 +0200

    maven project setup with hello message and a corresponding unit test

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(message());
+    }
+
+    public static String message() {
+        return "hello rot13";
+    }
+}
